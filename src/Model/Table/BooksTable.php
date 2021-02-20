2571a84749a15721be154f1fa348227c854a2f92
Stephan Steinkühler <st.stonecooler@gmail.com>
2021-02-20 22:26:16 +0100

    Init Commit inkl erste Books Model, Controller, View etc..

---
+++ src/Model/Table/BooksTable.php
@@ -0,0 +1,69 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Books Model
+ *
+ * @method \App\Model\Entity\Book newEmptyEntity()
+ * @method \App\Model\Entity\Book newEntity(array $data, array $options = [])
+ * @method \App\Model\Entity\Book[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Book get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Book findOrCreate($search, ?callable $callback = null, $options = [])
+ * @method \App\Model\Entity\Book patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Book[] patchEntities(iterable $entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Book|false save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Book saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Book[]|\Cake\Datasource\ResultSetInterface|false saveMany(iterable $entities, $options = [])
+ * @method \App\Model\Entity\Book[]|\Cake\Datasource\ResultSetInterface saveManyOrFail(iterable $entities, $options = [])
+ * @method \App\Model\Entity\Book[]|\Cake\Datasource\ResultSetInterface|false deleteMany(iterable $entities, $options = [])
+ * @method \App\Model\Entity\Book[]|\Cake\Datasource\ResultSetInterface deleteManyOrFail(iterable $entities, $options = [])
+ *
+ * @mixin \Cake\ORM\Behavior\TimestampBehavior
+ */
+class BooksTable extends Table
+{
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config): void
+    {
+        parent::initialize($config);
+
+        $this->setTable('books');
+        $this->setDisplayField('title');
+        $this->setPrimaryKey('id');
+
+        $this->addBehavior('Timestamp');
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator): Validator
+    {
+        $validator
+            ->integer('id')
+            ->allowEmptyString('id', null, 'create');
+
+        $validator
+            ->scalar('title')
+            ->maxLength('title', 256)
+            ->requirePresence('title', 'create')
+            ->notEmptyString('title');
+
+        return $validator;
+    }
+}
